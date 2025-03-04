0e9ed1c6f945acafba228a9d35a6ce3d6b460794
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-03-04 09:47:44 +0530

    adds parent and dot using script

--- src/chain_reaction.js
+++ src/chain_reaction.js
@@ -1,1 +1,36 @@
-window.onload = alert("HELLO WORLD!");
+const debug = function (arg) {
+  console.log(arg);
+  return arg;
+};
+
+const addStyles = (node, style) =>
+  Object.keys(style).map((key) => (node.style[key] = style[key]));
+
+const addAttributes = (node, attributes) => {
+  const { style, ...rest } = attributes;
+
+  if (style) addStyles(node, style);
+
+  Object.keys(rest).forEach((property) => {
+    node[property] = rest[property];
+  });
+};
+
+const createNode = (nodeName, attributes = {}, textContent = "") => {
+  const node = document.createElement(nodeName);
+
+  node.textContent = textContent;
+  addAttributes(node, attributes);
+
+  return node;
+};
+
+const startGame = () => {
+  const parent = createNode("div", { className: "parent" });
+  const dot = createNode("div", { className: "dot" });
+
+  parent.appendChild(dot);
+  document.body.appendChild(parent);
+};
+
+window.onload = startGame;
